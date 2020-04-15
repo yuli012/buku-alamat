bb364cd392540ef807014d08b24cca86b7d5ea92
yuli012 <yuli1800016012@webmail.uad.ac.id>
2020-04-15 05:31:16 -0700

    project buku alamat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//MATERIAL DESIGN COMPONENT
+import {MatToolbarModule} from '@angular/material/toolbar'; // tambahan baru
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { TambahAlamatComponent } from './tambah-alamat/tambah-alamat.component';
+import { DetailAlamatComponent } from './detail-alamat/detail-alamat.component';
+import { DialogKonfirmasiComponent } from './dialog-konfirmasi/dialog-konfirmasi.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TambahAlamatComponent,
+    DetailAlamatComponent,
+    DialogKonfirmasiComponent
   ],
+
+ 
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule, // tambahan baru
+    MatButtonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents:[
+    TambahAlamatComponent,
+    DetailAlamatComponent,
+    DialogKonfirmasiComponent
+  ]
 })
 export class AppModule { }
